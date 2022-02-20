ddbc22634ae2a738c3dcc0f13d0db3e109491716
joelgraff <monograff76@gmail.com>
2022-02-20 15:26:50 -0600

    add script

--- c64_dupe.sh
+++ c64_dupe.sh
@@ -10,11 +10,14 @@ disk_change () {
 
 get_dir () {
 
-	if cbmctrl dir 8 | tee -a "dir.log" | grep "read error"; then
-		echo -e "\n\nDisk Error"
+	if cbmctrl dir 8 | tee -a "dir.tmp" | grep "read error"; then
 		return -1
 	fi
-
+	
+	if cbmctrl dir 8 | tee -a "dir.tmp" | grep "drive not ready"; then
+		return -1
+	fi
+	
 	return 0
 }
 
@@ -44,36 +47,46 @@ make_image () {
 
 	filename=$image_name\_$disk_side
 
-	cbmctrl dir 8 >> ./$image_name/$filename.dir
+	mv dir.tmp ./$image_name/$filename.dir
 	d64copy 8 ./$image_name/$filename.d64
 }
 
 main () {
 
+	counter = 0
+	echo -e "\n\nInsert a disk and press a key to begin..."
+	
 	while [ $exit_program == "n" ]
 	do
-		echo -e "\n\nInsert disk side" $disk_side "and press a key (q to quit)..."
 		read -n 1 user_input
 
 		if [[ $user_input = "q" ]]; then
 			exit
 		fi
 
-		if [ $disk_side == "A" ]; then
-			get_dir
+		get_dir
 
-			if [ $? -eq 0 ]; then
-				make_path
-			fi
+		if [ $? -eq 0 ]; then
+			make_path
+			make_image
 		fi
 
-		make_image
-
+		echo -e "\n\nIMaging complete."
+		
+		if [[ $counter -gt 9 && $disk_side == "B" ]]; then
+			echo -e" \n\nINSERT DISK CLEANER AND PRESS A KEY"
+			cbmctrl dir 8
+		fi
+		
 		if [ $disk_side == "A" ]; then
 			disk_side="B"
+			echo -e "\n\nFlip disk and press a key (q to quit)..."
 		else
 			disk_side="A"
+			echo -e "\n\nInsert next disk and press a key (q to quit)..."
+			counter = $((counter + 1))
 		fi
+				
 	done
 }
 
