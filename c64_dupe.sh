4923d727825221a14f6f11625960ee8bc2676b84
Joel Graff <monograff76@gmail.com>
2022-02-20 22:44:14 -0600

    update

--- c64_dupe.sh
+++ c64_dupe.sh
@@ -1,7 +1,8 @@
-es#!/bin/bash
+#!/bin/bash
 exit_program="n"
 disk_side="A"
 image_name="."
+counter=0
 
 disk_change () {
 	echo -e "\n\nInsert disk (Side "$disk_side")"
@@ -13,11 +14,11 @@ get_dir () {
 	if cbmctrl dir 8 | tee -a "dir.tmp" | grep "read error"; then
 		return -1
 	fi
-	
+
 	if cbmctrl dir 8 | tee -a "dir.tmp" | grep "drive not ready"; then
 		return -1
 	fi
-	
+
 	return 0
 }
 
@@ -27,7 +28,8 @@ make_path () {
 
 	until [ ! -d ./$image_name ]
 	do
-		read -p "Enter image name:" image_name
+		echo -e "\n\n"
+		read -p "Enter image name: " image_name
 
 		if [ -d ./$image_name ]; then
 
@@ -45,48 +47,66 @@ make_path () {
 
 make_image () {
 
+	d64copy 8 ./image.d64
+	((counter=counter+1))
+}
+
+finalize () {
+
+	make_path
+
 	filename=$image_name\_$disk_side
 
 	mv dir.tmp ./$image_name/$filename.dir
-	d64copy 8 ./$image_name/$filename.d64
+	mv image.d64 ./$image_name/$filename.d64
 }
 
 main () {
 
-	counter = 0
 	echo -e "\n\nInsert a disk and press a key to begin..."
-	
+
 	while [ $exit_program == "n" ]
 	do
 		read -n 1 user_input
 
-		if [[ $user_input = "q" ]]; then
+		if [[ $user_input == "q" ]]; then
 			exit
 		fi
 
 		get_dir
 
 		if [ $? -eq 0 ]; then
-			make_path
 			make_image
 		fi
 
-		echo -e "\n\nIMaging complete."
-		
+		user_input="r"
+
+		while [[ $user_input == "r" ]]
+		do
+
+			echo -e "\n\n"
+			read -n 1 -p "(s)ave, (r)etry, (a)bort? " user_input
+
+			if [[ $user_input == "s" ]]; then
+				finalize
+			elif [[ $user_input == "r" ]]; then
+				make_image
+			fi
+		done
+
 		if [[ $counter -gt 9 && $disk_side == "B" ]]; then
 			echo -e" \n\nINSERT DISK CLEANER AND PRESS A KEY"
 			cbmctrl dir 8
 		fi
-		
+
 		if [ $disk_side == "A" ]; then
 			disk_side="B"
 			echo -e "\n\nFlip disk and press a key (q to quit)..."
 		else
 			disk_side="A"
 			echo -e "\n\nInsert next disk and press a key (q to quit)..."
-			counter = $((counter + 1))
 		fi
-				
+
 	done
 }
 
